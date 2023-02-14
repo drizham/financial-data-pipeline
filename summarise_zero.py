cc5dc4d2b36e6b43ae11c7c75a41167f868cdc18
Zack Izham <izham@gmx.net>
2023-02-14 15:47:56 +0100

    Add functionality for setting target location for saving results of describe_file

--- summarise_zero.py
+++ summarise_zero.py
@@ -1,18 +1,23 @@
 # apps to perform summarisation of data files
 
 import pandas as pd
+from utils_zero import create_directory
 
 
-def describe_file(file_path):
+def describe_file(input,output):
+    """Reads a csv (input) and runs a simple pandas describe on it
+    saves the data to (output) """
     try:
-        # run if file exists
-        d0 = pd.read_csv(file_path)
+        # run if input file exists
+        d0 = pd.read_csv(input)
     except FileNotFoundError:
-        msg = "Sorry, the file " + file_path + " does not exist."
+        msg = "Sorry, the file " + input + " does not exist."
         print(msg)
     else:
         # write the summary/ description file with _description appended to file name
-        path_to_write_to = file_path.split('.csv')[0] + '_description.csv'
-        d0.describe().to_csv(path_to_write_to)
-        msg = "Summary desicription written to: " + path_to_write_to
+        name_to_write_to = input.split('/')[-1]
+        create_directory(output) # creates directory if it does not exist
+        # TODO - add a try except to catch exeptions from pandas describe
+        d0.describe().to_csv(output + name_to_write_to)
+        msg = f'Summary written to {output + name_to_write_to}'
         print(msg)
