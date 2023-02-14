547c34e5f91b84bbd240d034739575cc2a2067eb
Zack Izham <izham@gmx.net>
2023-02-14 15:56:20 +0100

    Update terminology and function name to reflect all data in folder are deleted

--- manage_lake.py
+++ manage_lake.py
@@ -2,17 +2,18 @@
 import glob
 import os
 
-def remove_downloaded_csv():
-    # get list of data files previously downloaded
-    flist = glob.glob("data/week*.csv")
+def delete_files(path):
+    """Deletes files in a folder"""
+    # include / in path e.g. path = 'data/lake/folder_to_clear/'
+    flist = glob.glob(f'{path}week*.csv')
     number_of_files = len(flist)
     if (number_of_files):
-        print(str(number_of_files) + ' downloaded instrument data files found')
-        print('Deleting previous downloads')
-        # delete previously downloaded files 
+        print(str(number_of_files) + ' data files found')
+        print(f'Deleting files from {path}')
+        # delete files 
         for f in flist:
             fname = f.rstrip()
             if os.path.isfile(fname):
                 os.remove(fname)
     else:
-        print('No downloaded instrument data files found')
+        print('No data files found')
