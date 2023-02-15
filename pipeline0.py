c11619c906f418c82613e09db35991c8ebd0cf67
Zack Izham <izham@gmx.net>
2023-02-15 10:25:11 +0100

    Update pipeline0 with correct function calls to run the basic pipeline

--- pipeline0.py
+++ pipeline0.py
@@ -6,30 +6,32 @@ import download_zero as dz
 import manage_lake
 import summarise_zero as sz
 
-# remove all previously downloaded data
-manage_lake.remove_downloaded_csv()
+def main():
 
-# get start and end dates to download data for
-start_dates, end_dates = uz.read_start_end_dates('start_end_dates.csv')
+    # remove all previously downloaded data
+    manage_lake.delete_files('lake0/GC=F/raw/')
+    manage_lake.delete_files('lake0/GC=F/description/')
 
+    # get start and end dates to download data for
+    start_dates, end_dates = uz.read_start_end_dates('start_end_dates.csv')
 
-print('Running pipeline')
-time.sleep(2)
+    print('Starting pipeline')
+    time.sleep(1)
 
-# call first app in the pipeline
-# download financial data
-print('Downloading financial data files')
-dz.download_instrument_2_csv0('GC=F',start_dates, end_dates)
-time.sleep(2)
+    # call first app in the pipeline - download financial data
+    print('Downloading financial data files')
+    dz.download_instrument_2_csv0('GC=F',start_dates, end_dates, 'lake0/')
+    time.sleep(1)
 
-# call a 'summariser' that summarises each data *.csv file
-# use pandas in the summariser to calculate stats
-# and print a stats line per week of data
-print('Running basic describe on data files')
-flist = glob.glob("data/*.csv") # get the list of downloaded data
-for file in flist:
-    sz.describe_file(file)
+    # call a 'summariser' that summarises each data *.csv file
+    # use pandas in the summariser to calculate stats
+    print('Running basic describe on data files')
+    flist = glob.glob("lake0/GC=F/raw/*.csv") # get the list of downloaded data
+    for file in flist:
+        sz.describe_file(file, 'lake0/GC=F/description/')
 
-time.sleep(1)
+    time.sleep(1)
 
-print('Pipeline run completed')
+    print('Pipeline run completed')
+
+main()
