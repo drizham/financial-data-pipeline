483e4f89c9e698caf34c4adfcd216c1506543238
Zack Izham <izham@gmx.net>
2023-03-11 18:00:22 +0100

    Changed yfinance downloader to download hourly data for each week and save them to separate *.csv files

--- download_zero.py
+++ download_zero.py
@@ -13,6 +13,7 @@ def download_instrument_2_csv0(symbol,week_start_dates, week_end_dates,
     downloads and saves a few weeks of data as a single csv file in the folder"""
     #week_start_dates = ['2023-01-02', '2023-01-09', '2023-01-16', '2023-01-23', '2023-01-30']
     #week_end_dates = ['2023-01-07', '2023-01-14', '2023-01-21', '2023-01-28', '2023-02-04']
+    # yfinance intervals => 1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo
     # folder is the folder path to download to
 
     try:
@@ -28,7 +29,7 @@ def download_instrument_2_csv0(symbol,week_start_dates, week_end_dates,
                 print('Trying to download data for: ' + symbol)
                 obj0 = yf.Ticker(symbol)
                 #print('downloading data for week ' + str(week_count) + ' starting ' + week_start_dates[x] + ' ending ' + week_end_dates[x] )
-                d0 = obj0.history(interval='1d', start=week_start_dates[x], end=week_end_dates[x])
+                d0 = obj0.history(interval='1h', start=week_start_dates[x], end=week_end_dates[x])
                 if d0.empty:
                     # print('No data downloaded for symbol:' + symbol)
                     raise Exception('Symbol is either invalid or delisted')
