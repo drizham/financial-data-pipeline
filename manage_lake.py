2ab6465387ea9ce59b00fd88d19d832e9d6f0123
Zack Izham <izham@gmx.net>
2023-05-04 16:08:21 +0200

    fix manage_lake.py to print better logs

--- manage_lake.py
+++ manage_lake.py
@@ -3,9 +3,11 @@ import glob
 import os
 
 def delete_files(path):
-    """Deletes files in a folder"""
+    """Deletes all files in a folder"""
     # include / in path e.g. path = 'data/lake/folder_to_clear/'
-    flist = glob.glob(f'{path}week*.csv')
+    path_all = f'{path}*.*'
+    print(f'Checking path:{path_all} for files')
+    flist = glob.glob(path_all)
     number_of_files = len(flist)
     if (number_of_files):
         print(str(number_of_files) + ' data files found')
